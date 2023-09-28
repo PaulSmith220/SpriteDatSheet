5cc476a98af5cd3c76fe1242eaa5c25460045aa6
PaulSmith220 <kuznetps@gmail.com>
2023-09-28 12:23:27 +0200

    frame names trimming for auto-import in engines

--- spritesheeter/JSONformats.js
+++ spritesheeter/JSONformats.js
@@ -1,3 +1,6 @@
+function trimFrameName(name) {
+    return name.replace(/^\d{1,}_/, '');
+}
 
 function formatForPixieJS(data) {
     data = {
@@ -14,8 +17,9 @@ function formatForPixieJS(data) {
     json.animations.forEach(animation => {
         data.animations[animation.name] = [];
         animation.frames.forEach((frame) => {
-            data.animations[animation.name].push(frame.name);
-            data.frames[frame.name] = {
+            const frameName = trimFrameName(frame.name);
+            data.animations[animation.name].push(frameName);
+            data.frames[frameName] = {
                 frame: {
                     x: frame.x,
                     y: frame.y,
@@ -59,13 +63,17 @@ function formatForPhaser3(data) {
     const frames = {};
     data.animations.forEach(animation => {
         animation.frames.forEach(frame => {
-            frames[frame.name] = frame;
+            const frameName = trimFrameName(frame.name);
+            frames[frameName] = {
+                ...frame,
+                name: trimFrameName(frame.name),
+            };
         });
     });
 
     Object.values(frames).forEach(frame => {
         result.textures[0].frames.push({
-            filename: frame.name + '.png',
+            filename: trimFrameName(frame.name) + '.png',
             rotated: false,
             trimmed: false,
             sourceSize: {
@@ -88,7 +96,7 @@ function formatForPhaser3(data) {
     });
 
     data.animations.forEach(animation => {
-        result.textures[0].animations[animation.name] = animation.frames.map(f => f.name + '.png');
+        result.textures[0].animations[animation.name] = animation.frames.map(f => trimFrameName(f.name) + '.png');
     });
     return result;
 
@@ -112,13 +120,16 @@ function formatForGodot(data) {
     const frames = {};
     data.animations.forEach(animation => {
         animation.frames.forEach(frame => {
-            frames[frame.name] = frame;
+            frames[trimFrameName(frame.name)] = {
+                ...frame,
+                name: trimFrameName(frame.name),
+            };
         });
     });
 
     Object.values(frames).forEach(frame => {
         result.textures[0].sprites.push({
-            filename: frame.name + '.png',
+            filename: trimFrameName(frame.name) + '.png',
             region: {
                 x: frame.x,
                 y: frame.y,
@@ -135,7 +146,7 @@ function formatForGodot(data) {
     });
 
     data.animations.forEach(animation => {
-        result.textures[0].animations[animation.name] = animation.frames.map(f => f.name + '.png');
+        result.textures[0].animations[animation.name] = animation.frames.map(f => trimFrameName(f.name) + '.png');
     });
     return result;
 }
@@ -147,7 +158,10 @@ function formatForJSONArray(data) {
     const frames = {};
     data.animations.forEach(animation => {
         animation.frames.forEach(frame => {
-            frames[frame.name] = frame;
+            frames[trimFrameName(frame.name)] = {
+                ...frame,
+                name: trimFrameName(frame.name),
+            };
         });
     });
 
